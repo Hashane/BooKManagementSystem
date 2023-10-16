b763c02b399659cffffc2d30f9ab79e4c7623bed
Hashane <jhashane@gmail.com>
2023-10-17 02:07:58 +0530

    Dashboard ui improvements

--- resources/views/staff/dashboard.blade.php
+++ resources/views/staff/dashboard.blade.php
@@ -1,16 +1,31 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>Welcom, {{ Auth::user()->getRoleNames(); }}</h1>
-<p>This is the dashboard for {{ Auth::user()->hasRole('admin') ? 'Admin' : (Auth::user()->hasRole('editor') ? 'Editor' :
-    'Viewer') }}.</p>
+<div class="container">
+    <h1 class="mt-4">Welcome, {{ Auth::user()->name }}</h1>
+    <p>This is the dashboard for {{ Auth::user()->hasRole('admin') ? 'Admin' : (Auth::user()->hasRole('editor') ?
+        'Editor' : 'Viewer') }}.</p>
 
-@if (Auth::user()->hasRole('admin') || Auth::user()->hasRole('editor'))
-@include('books.index')
-@endif
+    @if (Auth::user()->hasRole('admin') || Auth::user()->hasRole('editor'))
+    <div class="row mt-4">
+        <div class="col-md-10">
+            @include('layouts.cards')
+        </div>
+        <!-- Add more cards for admin and editor functions -->
+    </div>
+    @endif
 
-@if (Auth::user()->hasRole('reader'))
-@include('borrowed_books')
-@include('history')
-@endif
+    @if (Auth::user()->hasRole('reader'))
+    <div class="mt-4">
+        <div class="row">
+            <div class="col-md-6">
+                @include('borrowed_books')
+            </div>
+            <div class="col-md-6">
+                @include('history')
+            </div>
+        </div>
+    </div>
+    @endif
+</div>
 @endsection
