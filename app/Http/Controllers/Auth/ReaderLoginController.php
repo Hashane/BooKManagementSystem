c8234492d3c97c0b6ffaa0d85ee4cb4a25f514d1
Hashane <jhashane@gmail.com>
2023-10-19 00:29:24 +0530

    Send custom failed login error message when account is deactivated

--- app/Http/Controllers/Auth/ReaderLoginController.php
+++ app/Http/Controllers/Auth/ReaderLoginController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\Reader;
+use Illuminate\Validation\ValidationException;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
@@ -26,6 +28,38 @@ class ReaderLoginController extends Controller
     protected function guard()
     {
         return Auth::guard('reader');
+    }
+
+    protected function credentials(Request $request)
+    {
+        return array_merge($request->only($this->username(), 'password'), ['active' => 1]);
+    }
+
+    /**
+     * Get the failed login response instance.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    protected function sendFailedLoginResponse(Request $request)
+    {
+        $errors = [$this->username() => trans('auth.failed')];
+
+        // Load user from database
+        $user = Reader::where($this->username(), $request->{$this->username()})->first();
+
+        // Check if user was successfully loaded, that the password matches
+        // and active is not 1. If so, override the default error message.
+        if ($user && \Hash::check($request->password, $user->password) && $user->active != 1) {
+            $errors = [$this->username() => trans('auth.inactive')];
+        }
+
+        if ($request->expectsJson()) {
+            return response()->json($errors, 422);
+        }
+        return redirect()->back()
+            ->withInput($request->only($this->username(), 'remember'))
+            ->withErrors($errors);
     }
 
     public function logout(Request $request)
---
+++ resources/lang/en/auth.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'failed' => 'These credentials do not match our records.',
+    'inactive' => 'This account has not been activated yet.',
+];
