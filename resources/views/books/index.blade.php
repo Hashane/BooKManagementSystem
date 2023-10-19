5c750ffc899f2a96d3f12166fdad83e336a03a49
Hashane <jhashane@gmail.com>
2023-10-20 03:20:51 +0530

    UI change

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -2,9 +2,14 @@
 
 @section('content')
 <div class="container">
-    @if (Auth::user()->hasRole('admin')) <a href="{{ route('books.create') }}" class="btn btn-success mb-4">Create New
-        Book</a> @endif
+    @if (Auth::user()->hasRole('admin'))
+    <a href="{{ route('books.create') }}" class="btn btn-success mb-4">
+        <i class="fas fa-plus"></i> Create New Book
+    </a>
+    @endif
+
     <h2 class="mb-4">Book List</h2>
+
     <div class="table-responsive">
         <table class="table table-striped table-hover">
             <thead>
@@ -22,17 +27,17 @@
                     <td>{{ $book->author }}</td>
                     <td>{{ $book->publication_year }}</td>
                     <td>
-                        @if (Auth::user()->hasRole('admin') || Auth::user()->hasRole('editor') )
-                        <a href="{{ route('books.assign-post', $book->id) }}" class="btn btn-primary" title="Assign">
-                            <i class="fa fa-tasks"></i>
+                        @if (Auth::user()->hasRole('admin') || Auth::user()->hasRole('editor'))
+                        <a href="{{ route('books.assign-post', $book->id) }}" class="btn btn-info" title="Assign">
+                            <i class="fa fa-tasks"></i> Assign
                         </a>
                         @endif
                         <a href="{{ route('books.show', $book->id) }}" class="btn btn-primary" title="View">
-                            <i class="fa fa-eye"></i>
+                            <i class="fa fa-eye"></i> View
                         </a>
-                        @if (Auth::user()->hasRole('admin') || Auth::user()->hasRole('editor') )
-                        <a href="{{ route('books.edit', $book->id) }}" class="btn btn-primary" title="Edit">
-                            <i class="fa fa-edit"></i>
+                        @if (Auth::user()->hasRole('admin') || Auth::user()->hasRole('editor'))
+                        <a href="{{ route('books.edit', $book->id) }}" class="btn btn-warning" title="Edit">
+                            <i class="fa fa-edit"></i> Edit
                         </a>
                         @endif
                         @if (Auth::user()->hasRole('admin'))
@@ -41,7 +46,7 @@
                             @method('DELETE')
                             <button type="submit" class="btn btn-danger" title="Delete"
                                 onclick="return confirm('Are you sure you want to delete this book?')">
-                                <i class="fa fa-trash"></i>
+                                <i class="fa fa-trash-alt"></i> Delete
                             </button>
                         </form>
                         @endif
