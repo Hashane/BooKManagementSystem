8f113ce9d802a0a27c12dbda0b2a8c1a100baf12
Hashane <jhashane@gmail.com>
2023-10-19 18:12:49 +0530

    Schedule backingup and restore task on every week

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,12 +7,18 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
+
+    protected $commands = [
+        \App\Console\Commands\DatabaseBackup::class,
+    ];
+
     /**
      * Define the application's command schedule.
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        //schedule
+        $schedule->command('database:backup')->weekly()->sundays()->at('00:00');
     }
 
     /**
@@ -20,7 +26,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands(): void
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
